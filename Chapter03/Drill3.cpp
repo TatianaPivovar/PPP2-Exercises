83d8af7442fef1ee18e63441a166498461528946
Tatiana Pivovarova <titan4ic@gmail.com>
2016-12-20 22:08:21 +0300

    drill 3.2
    solved drill 3.2

--- Chapter03/Drill3.cpp
+++ Chapter03/Drill3.cpp
@@ -3,8 +3,11 @@
 
 int main()
 {
-    std::cout << "Enter the name of the person you want to write to:\n";
+    std::cout << "Enter the name of the person you want to write to:" << std::endl;
     std::string first_name;
     std::cin >> first_name;
-    std::cout << "Dear " << first_name << ",\n";
+    std::cout << "Dear " << first_name << ',' << std::endl;
+    std::cout << "How are you? I an fine. I miss you." << std::endl;
+    std::cout << "I am reading the book about programming. It's pretty good."
+        << " And I will try to execute all exercises in this book." << std::endl;
 }
