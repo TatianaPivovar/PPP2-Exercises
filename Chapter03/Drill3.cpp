6deb4ba30d40341a13d91b0a1ac38c7fc1d178db
Tatiana Pivovarova <titan4ic@gmail.com>
2016-12-20 22:11:18 +0300

    drill 3.4
    the drill 4.4 solved

--- Chapter03/Drill3.cpp
+++ Chapter03/Drill3.cpp
@@ -11,9 +11,22 @@ int main()
     std::string friend_name;
     std::cin >> friend_name;
 
+    std::cout << "Is your friend is male or female? (enter 'm' or 'f'):" << std::endl;
+    char friend_sex = 0;
+    std::cin >> friend_sex;
+
+
     std::cout << "Dear " << first_name << ',' << std::endl;
     std::cout << "How are you? I an fine. I miss you." << std::endl;
     std::cout << "I am reading the book about programming. It's pretty good."
         << " And I will try to execute all exercises in this book." << std::endl;
     std::cout << "Have you seen " << friend_name << " lately?" << std::endl;
+    std::cout << "If you see " << friend_name << " please ask ";
+    if (friend_sex == 'm')
+        std::cout << "him ";
+    else if (friend_sex == 'f')
+        std::cout << "her ";
+    else
+        std::cout << "it ";
+    std::cout << "to call me." << std::endl;
 }
