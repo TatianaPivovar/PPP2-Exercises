84a5852b506a7e4421acb4cb0d4bb3a9ccb909a6
Tatiana Pivovarova <titan4ic@gmail.com>
2017-01-12 00:31:08 +0300

    minor enchancement

--- Chapter04/Drill.cpp
+++ Chapter04/Drill.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+void print_ordered(double v1, double v2)
+{
+	std::cout << "the smaller value is: " << v1 << std::endl;
+	std::cout << "the  larger value is: " << v2 << std::endl;
+}
+
 int main()
 {
     double v1 = 0, v2 = 0;
@@ -7,12 +13,10 @@ int main()
     std::cout << ">";
     while ((std::cin >> v1) && (std::cin >> v2)) {
 		if (v1 < v2) {
-			std::cout << "the smaller value is: " << v1 << std::endl;
-			std::cout << "the  larger value is: " << v2 << std::endl;
+			print_ordered(v1, v2);
 		}
 		else if (v2 < v1) {
-			std::cout << "the smaller value is: " << v2 << std::endl;
-			std::cout << "the  larger value is: " << v1 << std::endl;
+			print_ordered(v2, v1);
 		}
 		else {
 			std::cout << "the numbers are equal" << std::endl;
