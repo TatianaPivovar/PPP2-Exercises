607d590daa4e5efbe202ba60303fabe7ee532ab6
Tatiana Pivovarova <titan4ic@gmail.com>
2017-01-12 22:57:14 +0300

    drill 4.7

--- Chapter04/Drill.cpp
+++ Chapter04/Drill.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include <limits>
+#include <string>
 
 int main()
 {
-    std::cout << "Please enter two integer values (enter '|' to exit):" << std::endl;
+    std::cout << "Please enter a length followed by a unit (cm, m, in, ft or  '|' to exit):" << std::endl;
     std::cout << ">";
 	double input = 0;
 	double smallest = std::numeric_limits<double>::max();
 	double largest = std::numeric_limits<double>::min();
-    while ((std::cin >> input)) {
-		std::cout << "Entered: " << input;
+	std::string unit;
+    while ((std::cin >> input) && (std::cin >> unit)) {
+		std::cout << "Entered: " << input << unit;
+
+		if (unit == "cm") {
+			;
+		}
+		else if (unit == "m") {
+			std::cout << " = " << 100 * input << "cm";
+		}
+		else if (unit == "in") {
+			std::cout << " = " << 2.54 * input << "cm";
+		}
+		else if (unit == "ft") {
+			std::cout << " = " << 12 * input << "in";
+		}
+		else {}
+
 		if (input < smallest) {
 			std::cout << " the smallest so far";
 			smallest = input;
