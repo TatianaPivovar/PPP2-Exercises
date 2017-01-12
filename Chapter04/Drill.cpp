39eaa22351c360879f5febb8cac670360eb76207
Tatiana Pivovarova <titan4ic@gmail.com>
2017-01-12 22:57:43 +0300

    simple rename

--- Chapter04/Drill.cpp
+++ Chapter04/Drill.cpp
@@ -6,34 +6,34 @@ int main()
 {
     std::cout << "Please enter a length followed by a unit (cm, m, in, ft or  '|' to exit):" << std::endl;
     std::cout << ">";
-	double input = 0;
+	double value = 0;
 	double smallest = std::numeric_limits<double>::max();
 	double largest = std::numeric_limits<double>::min();
 	std::string unit;
-    while ((std::cin >> input) && (std::cin >> unit)) {
-		std::cout << "Entered: " << input << unit;
+    while ((std::cin >> value) && (std::cin >> unit)) {
+		std::cout << "Entered: " << value << unit;
 
 		if (unit == "cm") {
 			;
 		}
 		else if (unit == "m") {
-			std::cout << " = " << 100 * input << "cm";
+			std::cout << " = " << 100 * value << "cm";
 		}
 		else if (unit == "in") {
-			std::cout << " = " << 2.54 * input << "cm";
+			std::cout << " = " << 2.54 * value << "cm";
 		}
 		else if (unit == "ft") {
-			std::cout << " = " << 12 * input << "in";
+			std::cout << " = " << 12 * value << "in";
 		}
 		else {}
 
-		if (input < smallest) {
+		if (value < smallest) {
 			std::cout << " the smallest so far";
-			smallest = input;
+			smallest = value;
 		}
-		if (input > largest) {
+		if (value > largest) {
 			std::cout << " the largest so far";
-			largest = input;
+			largest = value;
 		}
         std::cout << std::endl << ">";
     }
