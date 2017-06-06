1e7dbd38f1c484ceeea08c24c9208f6f80ef1353
Tatiana Pivovarova <titan4ic@gmail.com>
2017-06-06 19:18:29 +0300

    Add description of the exercise 4.2

--- Chapter04/ex4.2.cpp
+++ Chapter04/ex4.2.cpp
@@ -1,3 +1,10 @@
+/*
+If we define the median of a sequence as “a number so that exactly as many
+elements come before it in the sequence as come after it,” fix the program
+in §4.6.3 so that it always prints out a median. Hint: A median need not be
+an element of the sequence.
+*/
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
